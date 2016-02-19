0bd53ae5cde5bae70c15595133facaf04a612f08
Eric Luan <eluan@140-sfoengwifi53-131.clients.corp.yelpcorp.com>
2016-02-19 14:15:33 -0800

    Create table schemas for items and comments

--- server/sql/create_tables.sql
+++ server/sql/create_tables.sql
@@ -4,8 +4,36 @@ DROP DATABASE IF EXISTS hoot;
 CREATE SCHEMA IF NOT EXISTS hoot;
 USE hoot;
 
-CREATE TABLE user(
-    user_id int NOT NULL AUTO_INCREMENT, 
-    name varchar(100) NOT NULL,
-    PRIMARY KEY (user_id)
+CREATE TABLE item(
+    category varchar(100) NOT NULL,
+    id varchar(100) NOT NULL,
+    name varchar(500),
+    token varchar(100),
+    urls varchar(2500), 
+    PRIMARY KEY(category,id)
 );
+
+CREATE TABLE review(
+    id int NOT NULL, 
+    item_category varchar(100) NOT NULL,
+    item_id varchar(100) NOT NULL, 
+    is_positive boolean,
+    comment_token varchar(100),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE item_tag(
+    id int NOT NULL, 
+    tag varchar(50),
+    item_category varchar(100) NOT NULL,
+    item_id varchar(100) NOT NULL, 
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE comment_tag(
+    id int NOT NULL, 
+    tag varchar(50), 
+    comment_id int NOT NULL, 
+    PRIMARY KEY(id)
+);
+
