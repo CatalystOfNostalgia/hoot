f2d1b4e3007fb637247171f8ae5ada2892d71f7a
Anthony Dario <anthonyrdario@gmail.com>
2016-03-13 14:11:09 -0400

    triggers update emotions tables when others are inserted into

--- server/sql/create_tables.sql
+++ server/sql/create_tables.sql
@@ -10,18 +10,18 @@ CREATE TABLE multimedia (
     creator varchar(100) NOT NULL,
     description varchar (100),
     media_type varchar (100) NOT NULL,
-    number_of_comments int NOT NULL,
+    number_of_comments int NOT NULL DEFAULT 0,
     PRIMARY KEY (media_id)
 );
 
-CREATE TABLE multimediaemotions (
-    media_id int NOT NULL,
-    pleasantness double NOT NULL,
-    attention double NOT NULL,
-    sensitivity double NOT NULL,
-    aptitude double NOT NULL,
-    polarity double NOT NULL,
-    PRIMARY KEY (media_id)
+CREATE TABLE multimedia_emotions (
+    media_id int NOT NULL AUTO_INCREMENT,
+    pleasantness double,
+    attention double,
+    sensitivity double,
+    aptitude double,
+    polarity double,
+    PRIMARY KEY (media_id),
     FOREIGN KEY (media_id) REFERENCES multimedia(media_id)
 );
 
@@ -32,8 +32,8 @@ CREATE TABLE comments (
     FOREIGN KEY (item_id) REFERENCES multimedia(media_id)
 );
 
-CREATE TABLE commentemotions (
-    comment_id int NOT NULL,
+CREATE TABLE comment_emotions (
+    comment_id int NOT NULL AUTO_INCREMENT,
     relevancy_score int NOT NULL,
     pleasantness double NOT NULL,
     attention double NOT NULL,
@@ -47,13 +47,26 @@ CREATE TABLE commentemotions (
 
 -- Triggers
 
--- update number of comments for a media whenever a comment is inserted
 delimiter |
 
+-- update number of comments for a media whenever a comment is inserted
 CREATE TRIGGER update_num_comments AFTER INSERT ON comments
     FOR EACH ROW
     BEGIN
         UPDATE multimedia SET number_of_comments = number_of_comments + 1 WHERE media_id = NEW.item_id;
+        INSERT INTO comment_emotions () VALUES ();
     END;
 
 |
+
+-- new media entry -> new media_emotion entry
+CREATE TRIGGER add_multimedia_emotions AFTER INSERT ON multimedia
+    FOR EACH ROW
+    BEGIN
+        INSERT INTO multimedia_emotions () VALUES ();
+    END;
+
+|
+            
+
+delimiter ;
