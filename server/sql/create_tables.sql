4cdc68633923f7f1d0c2b5c6054e4988c9b7bf7b
Anthony Dario <anthonyrdario@gmail.com>
2016-03-13 13:47:03 -0400

    moved vector representations to different tables

--- server/sql/create_tables.sql
+++ server/sql/create_tables.sql
@@ -11,17 +11,29 @@ CREATE TABLE multimedia (
     description varchar (100),
     media_type varchar (100) NOT NULL,
     number_of_comments int NOT NULL,
+    PRIMARY KEY (media_id)
+);
+
+CREATE TABLE multimediaemotions (
+    media_id int NOT NULL,
     pleasantness double NOT NULL,
     attention double NOT NULL,
     sensitivity double NOT NULL,
     aptitude double NOT NULL,
     polarity double NOT NULL,
     PRIMARY KEY (media_id)
+    FOREIGN KEY (media_id) REFERENCES multimedia(media_id)
 );
 
 CREATE TABLE comments (
     comment_id int NOT NULL AUTO_INCREMENT,
     item_id int NOT NULL,
+    PRIMARY KEY (comment_id),
+    FOREIGN KEY (item_id) REFERENCES multimedia(media_id)
+);
+
+CREATE TABLE commentemotions (
+    comment_id int NOT NULL,
     relevancy_score int NOT NULL,
     pleasantness double NOT NULL,
     attention double NOT NULL,
@@ -29,7 +41,8 @@ CREATE TABLE comments (
     aptitude double NOT NULL,
     polarity double NOT NULL,
     PRIMARY KEY (comment_id),
-    FOREIGN KEY (item_id) REFERENCES multimedia(media_id)
+    FOREIGN KEY (comment_id) REFERENCES comments(comment_id)
+
 );
 
 -- Triggers
