d68887d5229a3e0c8f4c6193d3432bc52030048d
Anthony Dario <anthonyrdario@gmail.com>
2016-03-23 11:30:11 -0400

    Finds longest emotion!

--- server/hoot/emotions.py
+++ server/hoot/emotions.py
@@ -1,7 +1,9 @@
 import sys
+import string
 
+# search for a concept given a certain string
 def concept_search(query, start):
-    f = open("output.txt", 'r')
+    f = open('concepts.txt', 'r')
     num = 0
     output = ''
 
@@ -13,6 +15,7 @@ def concept_search(query, start):
 
     return (-1, "NO CONCEPT")
             
+# find all the concepts for a comment
 def find_concepts(comment, start):
     words = comment.split()
 
@@ -20,26 +23,45 @@ def find_concepts(comment, start):
 
     for i in range(0, len(words)): 
         concept = words[i]
+        last_concept = None
         print("looking at: " + concept)
         line, found_concept = concept_search(words[i], start)
         print("found concept: " + found_concept)
         j = i + 1
 
-        while not found_concept == concept and not line == -1 and j < len(words):
-            print("entered loop")
-            concept = concept + words[j]
-            print("looking at: " + concept)
-            j = j + 1
-            line, found_concept = concept_search(words[i], line)
-            print("found concept: " + found_concept)
         if found_concept == concept:
-            output.append(found_concept)
+            print('\tlast concept = ' + found_concept)
+            last_concept = found_concept
 
+        while not line == -1 and j < len(words):
+            concept = concept + '_' + words[j]
+            print("\tlooking at: " + concept)
+            j = j + 1
+            line, found_concept = concept_search(concept, line)
+            print("\tfound concept: " + found_concept)
+            print('\tlooking for:   ' + concept)
+            if found_concept == concept:
+                print('\tlast concept = ' + found_concept)
+                last_concept = found_concept
+        
+        if last_concept != None:
+            print('appending: ' + last_concept)
+            output.append(last_concept)
+            
     if len(output) == 0:
         return "no concepts found"
     return output
 
-concepts = find_concepts(" ".join(sys.argv), 2)
+#print(concept_search(sys.argv[1], 0))
+#concepts = find_concepts(" ".join(sys.argv), 2)
+comment = "What has been said about the Dark Knight cannot be elaborated on - so I won't. The film is muscling its way into my #1 favorite comic movie adaptation of all time. The reason for my review is in hopes of saving you some money. This double disc Special Edition doesn't deliver the price you pay for it. There isn't even deleted scenes!!! I would save your very hard earned dollars and buy the single disc version and wait for the inevitable ULTIMATE re-release that will come later on down the road. But nonetheless, a great film - you will not be dissapointed; I just wish the studio would have given a better Special Edition release than what we have here. So enjoy!"
+
+comment = comment.translate(string.maketrans("",""), string.punctuation)
+comment = comment.lower()
+
+concepts = find_concepts(comment, 2)
 print("concepts: " + str(concepts))
+
+
 
 
