aca466ed36a08b8bca26fe6fe80b91435b0f90b9
Eric Luan <efl11@case.edu>
2016-04-12 14:03:31 -0400

    Made some more things we will eventually need

---
+++ Hoot/Hoot/ReviewsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ReviewsViewController.swift
+//  Hoot
+//
+//  Created by Eric Luan on 4/12/16.
+//  Copyright © 2016 Eric Luan. All rights reserved.
+//
+
+import UIKit
+
+class ReviewsViewController: UITableViewController {
+
+}
