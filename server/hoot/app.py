152eb46fa45759348ca8b3fda5c07126b9d31b70
Anjana <axr491@case.edu>
2016-04-17 19:40:28 -0400

    search params work

--- server/hoot/app.py
+++ server/hoot/app.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python
-from flask import Flask, jsonify
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
+api = Api(app)
 
 @app.route('/', methods=['GET'])
 def index():
     return 'You are being greeted by hoot!'
 
-@app.route('/hoot/api/v1.0/', methods=['GET'])
-def v1_index():
-    return jsonify({'hello': 'Hello World!'})
+class MultimediaAPI(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('product', type=str)
+        parser.add_argument('emotion', type=str)
 
+        return parser.parse_args()
+
+api.add_resource(MultimediaAPI, '/search', endpoint='search')
 
 if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=80)
-
-
-
-
+    #app.run(host='0.0.0.0', port=80)
+    app.run(debug=True)
