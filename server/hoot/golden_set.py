8f930e0369bd9d5d300165c6fe3954628e600b4f
Anthony Dario <anthonyrdario@gmail.com>
2016-04-20 02:01:26 -0400

    prettier printing
    more confident that this method is not giving a good average of
    the sentics.

--- server/hoot/golden_set.py
+++ server/hoot/golden_set.py
@@ -10,18 +10,22 @@ with open('../senticnet3.rdf.xml', 'r') as f:
     g.parse(f)
 
 for comment in data['comments']:
-   emotion   = emotions(comment, g)
-   sentics   = emotion.get_all_sentic_values()
-   compounds = emotion.get_compound_emotion()
+    emotion   = emotions(comment, g)
+    sentics   = emotion.get_all_sentic_values()
+    compounds = emotion.get_compound_emotion()
 
-   print(comment[0:60])
-   print('\temotion vector:')
-   print('\t\t{}'.format(emotion.emotion_vector))
+    print(comment[0:60])
 
-   print('\tsentic values:')
-   for sentic in sentics:
-      print('\t\t{}'.format(sentic.name)) 
+    print('\temotion vector:')
+    for emo in emotion.emotion_vector:
+        print('\t\t{}: {}'.format(emo, emotion.emotion_vector[emo]))
 
-   print('\tcompound_emotion:')
-   for compound in compounds:
-       print('\t\t{}'.format(compound.name))
+    print('\tsentic values:')
+    for sentic in sentics:
+        print('\t\t{}'.format(sentic.name)) 
+
+    print('\tcompound_emotion:')
+    for compound in compounds:
+        print('\t\t{}'.format(compound.name))
+    
+    print('\n')
