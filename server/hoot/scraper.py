5e86a078834dfb0d156fe2bcc0cee05781e00611
root <root@ip-172-31-20-211.us-west-2.compute.internal>
2016-04-22 23:52:57 +0000

    scraper takes in all the asins

--- server/hoot/scraper.py
+++ server/hoot/scraper.py
@@ -1,38 +1,93 @@
 import sys, os, json
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
 from amazon_scraper import AmazonScraper
-from amazon import api
+import url_scrape
 import calendar
-from datetime import datetime
 import queries
 import time
 import datetime
-import url_scrape
 
-#update all asins
-def test_scrape(asin_list):
-    f = open(os.path.dirname(os.path.realpath(__file__)) + "/keys/aws_keys.json")
-    configs = json.loads(f.read())
+def update_reviews(asin_list):
     for asin in asin_list:
+        print(asin)
+        f = open(os.path.dirname(os.path.realpath(__file__)) + "/keys/aws_keys.json")
+        configs = json.loads(f.read())
         amzn = AmazonScraper(configs["aws_public_key"], configs["aws_secret_key"], configs["product_api_tag"])
-        p = amzn.lookup(ItemId=asin)
+        try:
+            p = amzn.lookup(ItemId=asin)
+        except amazon.api.AsinNotFound as e:
+            continue
         reviews = p.reviews()
-        all_reviews = list(reviews)
-        for review in all_reviews:
-            print(asin)
-            print(review.url)
-            d = review.date
-            unix_time = calendar.timegm(d.utctimetuple())
-            print(unix_time)
-            print(url_scrape.parser(review.url))
-            # for review in reviews:
-            #     d = review.date
-            #
-            #     print(unix_time)
-                #print(review.text)
+        dates = queries.find_date_for_review(asin)
+        media_type = queries.find_type_by_id(asin)
+        unix_dates = []
+        for date in dates:
+            unix_dates.append(date)
+        date = max(unix_dates)
+        update = False
+        for review in reviews:
+            if date < get_date(review.date):
+                print("needs updating")
+                update = True
+        list_of_review_dicts =[]
+        #if the product has new reviews get them from amazon
+        if(update):
+            all_reviews = list(reviews)
+            for review in all_reviews:
+                print(asin)
+                print(review.url)
+                d = review.date
+                unix_time = calendar.timegm(d.utctimetuple())
+                print(unix_time)
+                print(url_scrape.parser(review.url))
+
+                 # product_api = aws_module.setup_product_api()
+                 # comment_dict = dict()
+                 # comment_dict["text"] = review.text
+                 # comment_dict["unixtime"] = int(review.date)
+                 # list_of_review_dicts.append(comment_dict)
+        #return data_ingester.handleReview(asin, list_of_review_dicts, product_api, media_type)
+
+def get_date(d):
+    unix_time = calendar.timegm(d.utctimetuple())
+    return unix_time
+
+def get_asins():
+    """
+    gets all asins and calls update
+    """
+    media = queries.get_all_media()
+    asins = []
+    for item in media:
+        asins.append(item.asin)
+    for asin in asins:
+        asins.pop()
+        update_reviews(asins)
+
+# def test_scrape(asin):
+#     f = open(os.path.dirname(os.path.realpath(__file__)) + "/keys/aws_keys.json")
+#     configs = json.loads(f.read())
+#     for asin in asin_list:
+#         amzn = AmazonScraper(configs["aws_public_key"], configs["aws_secret_key"], configs["product_api_tag"])
+#         p = amzn.lookup(ItemId=asin)
+#         reviews = p.reviews()
+#         all_reviews = list(reviews)
+#         for review in all_reviews:
+#             print(asin)
+#             print(review.url)
+#             d = review.date
+#             unix_time = calendar.timegm(d.utctimetuple())
+#             print(unix_time)
+#             print(url_scrape.parser(review.url))
+#         # for review in reviews:
+#         #     d = review.date
+#         #
+#         #     print(unix_time)
+#             #print(review.text)
 
 if __name__ == '__main__':
-   test_scrape(["0310893984","0740319116", "0766233995"])
+    get_asins()
     #Scraper.get_asins()
+
 
 
