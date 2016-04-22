d404efccae0aed9ed2045cac9d17f4d5a2ef5583
Anthony Dario <anthonyrdario@gmail.com>
2016-04-22 01:40:57 -0400

    review stars work
    Got rid of some more images.
    
    This could also probably look better but its late...

--- Hoot/Hoot/ReviewViewController.swift
+++ Hoot/Hoot/ReviewViewController.swift
@@ -25,5 +25,78 @@ class ReviewViewController: UIViewController {
         
         commentTextView.text = comment!.comment
         relevancyLabel.text = "Relevancy: \(comment!.relevancy)"
+        var images = getImages()
+        
+        firstStarView.image = images[0]
+        secondStarView.image = images[1]
+        thirdStarView.image = images[2]
+        fourthStarView.image = images[3]
+        fifthStarView.image = images[4]
+    }
+    
+    
+    func getImages() -> [UIImage] {
+        var images = [UIImage](count: 5, repeatedValue: UIImage(named: "31x31_0")!)
+        
+//        for i in 0...4 {
+//            images[i] = UIImage(named: "31x31_0")!
+//        }
+        
+        print("rating \(comment!.rating)")
+        switch(comment!.rating) {
+        case 1..<1.5:
+            images[0] = UIImage(named: "31x31_1")!
+            images[1] = UIImage(named: "31x31_1-5")!
+            break
+        case 1.5..<2:
+            images[0] = UIImage(named: "31x31_1")!
+            images[1] = UIImage(named: "31x31_1")!
+            break
+        case 2..<2.5:
+            images[0] = UIImage(named: "31x31_2")!
+            images[1] = UIImage(named: "31x31_2")!
+            images[2] = UIImage(named: "31x31_2-5")!
+            break
+        case 2.5..<3:
+            images[0] = UIImage(named: "31x31_2")!
+            images[1] = UIImage(named: "31x31_2")!
+            images[2] = UIImage(named: "31x31_2")!
+            break
+        case 3..<3.5:
+            images[0] = UIImage(named: "31x31_3")!
+            images[1] = UIImage(named: "31x31_3")!
+            images[2] = UIImage(named: "31x31_3")!
+            images[3] = UIImage(named: "31x31_3-5")!
+            break
+        case 3.5..<4:
+            images[0] = UIImage(named: "31x31_3")!
+            images[1] = UIImage(named: "31x31_3")!
+            images[2] = UIImage(named: "31x31_3")!
+            images[3] = UIImage(named: "31x31_3")!
+            break
+        case 4..<4.5:
+            images[0] = UIImage(named: "31x31_4")!
+            images[1] = UIImage(named: "31x31_4")!
+            images[2] = UIImage(named: "31x31_4")!
+            images[3] = UIImage(named: "31x31_4")!
+            images[4] = UIImage(named: "31x31_4-5")!
+            break
+        case 4.5..<5:
+            images[0] = UIImage(named: "31x31_4")!
+            images[1] = UIImage(named: "31x31_4")!
+            images[2] = UIImage(named: "31x31_4")!
+            images[3] = UIImage(named: "31x31_4")!
+            images[4] = UIImage(named: "31x31_4")!
+            break
+        default:
+            images[0] = UIImage(named: "31x31_5")!
+            images[1] = UIImage(named: "31x31_5")!
+            images[2] = UIImage(named: "31x31_5")!
+            images[3] = UIImage(named: "31x31_5")!
+            images[4] = UIImage(named: "31x31_5")!
+            break
+        }
+        
+        return images
     }
 }
